326dc960d07b63717c13c64bfe7fa52cac95d4d1
Induwara <induwara2k@gmail.com>
2023-09-28 19:52:49 +0530

    created Ai player

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -8,6 +8,22 @@ public class AiPlayer extends Player{
     @Override
     public void movePiece(int col) {
 
+        do {
+            int randomNum = (int)(Math.random() * 6);
+            if(board.isLegalMove(randomNum)){
+                col=randomNum;
+                break;
+            }
+        }while (true);
+        board.updateMove(col,Piece.BLUE);
+        board.getBoardUI().update(col,false);
+        Winner winner=board.findWinner();
+        if (winner.getWinningPiece()!=Piece.EMPTY){
+            board.getBoardUI().notifyWinner(winner);
+        }
+        else if (!board.existLegalMoves()){
+            board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+        }
 
 
     }
