e2caa28ec9cbe434f2c19f1f42fb72210c4a43e2
Induwara <induwara2k@gmail.com>
2023-10-06 04:34:16 +0530

    add other classes to AiPlayer class as static classes

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep.service;
 
+import java.util.*;
+
 public class AiPlayer extends Player{
     public AiPlayer(Board board) {
         super(board);
@@ -32,4 +34,281 @@ public class AiPlayer extends Player{
 
 
     }
+
+    //MCTS Algorithm itself and some Utility Classes
+    public static class MCTS {
+
+        private final Board board;
+
+
+        private final int computations;
+
+        public MCTS(Board board, int computations) {
+            this.board = board;
+            this.computations = computations;
+        }
+
+        public int findTheMove(){
+            int count=0;
+
+            Node tree= new Node(board,Piece.BLUE);
+
+            while (count<computations){
+
+                //Selection
+                Node selectedNode=selectNode(tree);
+
+                //Expand
+                Node nodeToExplore= expandNode(selectedNode);
+
+                //Simulation
+                Piece result=randomSimulation(nodeToExplore);
+
+                //Backpropagation
+                backPropagation(nodeToExplore,result);
+
+
+                count++;
+            }
+
+            Node bestNode=tree.getChildWithMaxScore();
+
+            int move=bestNode.move;
+
+            return move;
+
+        }
+
+        private void backPropagation(Node nodeToExplore, Piece result) {
+
+            Node node=nodeToExplore;
+            while (node!=null){
+                node.visit++;
+                if (node.piece==result){
+                    node.score++;
+                }
+                node=node.parent;
+            }
+
+        }
+
+        private Piece randomSimulation(Node nodeToExplore) {
+            Board board=copyBoardState(nodeToExplore.board);
+            Node node= new Node(board,nodeToExplore.piece);
+            node.parent=nodeToExplore.parent;
+
+            //System.out.println(node.parent);
+            if (node.board.findWinner().getWinningPiece()==Piece.BLUE){
+                node.parent.score=Integer.MIN_VALUE;
+                return Piece.BLUE;
+            }
+
+            while (isTheGameOngoing(node.board)){
+                BoardWithIndex nextMove=getRandomNextBoard(node);
+                Node child=new Node(nextMove.getBoard(),node.piece);
+                child.parent=node;
+                child.move=nextMove.getIndex();
+                node.addChild(child);
+                node = child;
+            }
+
+            if (node.board.findWinner().getWinningPiece()==Piece.GREEN){
+                return Piece.GREEN;
+            }
+            else if (node.board.findWinner().getWinningPiece()==Piece.BLUE){
+                return Piece.BLUE;
+            }
+            else {
+                return Piece.EMPTY; //Draw
+            }
+        }
+
+        private BoardWithIndex getRandomNextBoard(Node node) {
+            List<BoardWithIndex> legalMoves=getLegalMoves(node);
+            Random random=new Random();
+            //Maybe If method can be here
+            if (legalMoves.isEmpty()) {
+                return null;
+            }
+            int randomIndex=random.nextInt(legalMoves.size());
+            return legalMoves.get(randomIndex);
+        }
+
+
+        private Node expandNode(Node selectedNode) {
+
+            boolean gameStatus=isTheGameOngoing(selectedNode.board); //True //Flase
+            if (!gameStatus){
+                return selectedNode;
+            }
+            else {
+                List<BoardWithIndex> nextLegalMoves=getLegalMoves(selectedNode);
+                for (int i = 0; i < nextLegalMoves.size(); i++) {
+                    Board move=nextLegalMoves.get(i).getBoard();
+                    Node childNode=new Node(move,(selectedNode.piece==Piece.BLUE)?Piece.GREEN:Piece.BLUE);
+                    childNode.parent=selectedNode;
+                    childNode.move=nextLegalMoves.get(i).getIndex();
+                    selectedNode.addChild(childNode);
+                }
+                Random random=new Random();
+                int randomIndex=random.nextInt(nextLegalMoves.size());
+                return selectedNode.children.get(randomIndex);
+            }
+        }
+
+        public List<BoardWithIndex> getLegalMoves(Node selectedNode) {
+
+            Node node=selectedNode;
+            List<BoardWithIndex> nextMoves = new ArrayList<>();
+
+            //Find the next Player
+            Piece nextPiece= (selectedNode.piece==Piece.BLUE)?Piece.GREEN:Piece.BLUE;
+
+            for (int i = 0; i < 6; i++) {
+                if (node.board.isLegalMove(i)){
+                    int raw=node.board.findNextAvailableSpot(i);
+                    Board copyBoard=copyBoardState(node.board);
+                    copyBoard.updateMove(i,raw,nextPiece);
+                    BoardWithIndex boardWithIndex=new BoardWithIndex(copyBoard,i);
+                    nextMoves.add(boardWithIndex);
+                    // System.out.println("Moves: "+i);
+                    //System.out.println(Arrays.deepToString(copyBoard.getPieces()));
+                }
+            }
+
+            return nextMoves;
+        }
+
+
+
+
+        public Node selectNode(Node tree) {
+            Node currentNode=tree;
+            while (currentNode.children.size()!=0){
+                currentNode=UCT.findBestNodeWithUCT(currentNode);
+            }
+            return currentNode;
+        }
+
+
+
+        public boolean isTheGameOngoing(Board board){
+            Winner winner=board.findWinner();
+            if (winner.getWinningPiece()!=Piece.EMPTY){
+                return false;
+            } else if (!board.existLegalMoves()) {
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+        private Board copyBoardState(Board originalBoard) {
+            // Create a new board and copy the state cell by cell
+            Board newBoard = new BoardImpl(originalBoard.getBoardUI());
+            for (int col = 0; col < Board.NUM_OF_COLS; col++) {
+                for (int row = 0; row < Board.NUM_OF_ROWS; row++) {
+                    Piece piece = originalBoard.getPieces()[col][row];
+                    newBoard.updateMove(col, row, piece);
+                }
+            }
+            return newBoard;
+        }
+
+
+
+
+    }
+
+    //To Store Board Type Objects and their indexes
+    public static class BoardWithIndex {
+        private Board board;
+        private int index;
+
+        public BoardWithIndex(Board board, int index) {
+            this.board = board;
+            this.index = index;
+        }
+
+        public Board getBoard() {
+            return board;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        @Override
+        public String toString() {
+            return "BoardWithIndex{" +
+                    "board=" + board +
+                    ", index=" + index +
+                    '}';
+        }
+
+
+
+    }
+
+    //Node
+    public static class Node{
+        public Board board;
+
+        public int visit;
+
+        public int score;
+
+        List<Node> children = new ArrayList<>();
+
+        Node parent= null;
+
+        public Piece piece;
+
+        public int move;
+
+        public Node(Board board, Piece piece) {
+            this.board = board;
+            this.piece = piece;
+        }
+
+        public Node getChildWithMaxScore() {
+            Node result = children.get(0);
+            for (int i = 1; i < children.size(); i++) {
+                if (children.get(i).score > result.score) {
+                    result = children.get(i);
+                }
+            }
+            return result;
+        }
+
+        public void addChild(Node node) {
+            children.add(node);
+        }
+    }
+
+    //The UTC Finder
+    public static class UCT {
+
+        public static double uctValue(
+                int totalVisit, double nodeWinScore, int nodeVisit) {
+            if (nodeVisit == 0) {
+                return Integer.MAX_VALUE;
+            }
+            return ((double) nodeWinScore / (double) nodeVisit)
+                    + 1.41 * Math.sqrt(Math.log(totalVisit) / (double) nodeVisit);
+        }
+
+        public static Node findBestNodeWithUCT(Node node) {
+            int parentVisit = node.visit;
+            return Collections.max(
+                    node.children,
+                    Comparator.comparing(c -> uctValue(parentVisit,
+                            c.score, c.visit)));
+        }
+    }
+
+
+
 }
