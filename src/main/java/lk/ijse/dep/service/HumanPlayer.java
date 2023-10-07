d47a71faee21ce93984ed6a2d771ac186eb4189c
Induwara <induwara2k@gmail.com>
2023-10-07 18:33:11 +0530

    add testing v2

--- src/main/java/lk/ijse/dep/service/HumanPlayer.java
+++ src/main/java/lk/ijse/dep/service/HumanPlayer.java
@@ -8,6 +8,7 @@ public class HumanPlayer extends Player{
     @Override
     public void movePiece(int col) {
 
+        //This for testing
         if (board.isLegalMove(col)) {
             board.updateMove(col, Piece.BLUE);
             board.getBoardUI().update(col, true);
